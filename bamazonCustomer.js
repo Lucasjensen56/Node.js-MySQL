207885ea6e6dff04adfeea6de082f8cbe7cbbc6a
Lucas Jensen <lucasjensen56@gmail.com>
2018-04-27 15:27:17 -0500

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,23 @@
 
+var inquirer = require("requirer");
+
+inquirer.prompt([
+  
+  {
+    type: "input",
+    name: "product_ID",
+    message: "What is the product ID you woud like to buy?"
+  },
+  
+  {
+    type: "input",
+    name: "Number of product to buy?",
+    message: "How many would you like to buy?"
+  },
+  
+  ]).then(function(user) {
+  
+  
+  
+  
+});
