e0d94639c9b155402bfdb14c9f574053f6727147
Lucas Jensen <lucasjensen56@gmail.com>
2018-04-27 12:34:07 -0500

    Update bamazon.js

--- bamazon.js
+++ bamazon.js
@@ -16,12 +16,12 @@ INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`,
 INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Macbook Pro", "Electronics","1399.00", '50');
 INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Apple Watch", "Electronics","325.00", '10');
 INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Power Cord", "Electronics","50.00", '5');
-INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iPhone", "Electronics","900.00", '100');
-INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iPhone", "Electronics","900.00", '100');
-INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iPhone", "Electronics","900.00", '100');
-INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iPhone", "Electronics","900.00", '100');
-INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iPhone", "Electronics","900.00", '100');
-INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iPhone", "Electronics","900.00", '100');
-INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iPhone", "Electronics","900.00", '100');
+INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "iMac", "Electronics","1800.00", '2');
+INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Protein Powder", "Health and Fitness","31.00", '20');
+INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Pre-Workout", "Health and Fitness","20.00", '100');
+INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Men's Shorts", "Health and Fitness","30.00", '200');
+INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Tank Top", "Health and Fitness","10.00", '34');
+INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Running Shoes", "Health and Fitness","79.00", '10');
+INSERT INTO `products` (`id`,`item_id`,`product_name`,`department_name`,`price`, `stock_quantity`) VALUES (NULL, "Protein bar", "Health and Fitness","1.99", '5');
 
 
